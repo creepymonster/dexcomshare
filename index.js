2dac97f8a6686997b40414f7288201cce5651f0a
Reimar Metzen <reimar.metzen@iti-software.de>
2020-01-31 09:31:47 +0100

    fix heroku start

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ fastify.get('/', function (request, reply) {
 });
 
 // Run the server!
-fastify.listen(PORT, function (err, address) {
+fastify.listen(PORT, '0.0.0.0', function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
   fastify.log.info(`server listening on ${address}`)
-})
+});
