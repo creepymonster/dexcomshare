c46f9eaec9d98a6982e6f518780cd0a7783e1667
Reimar Metzen <reimar.metzen@iti-software.de>
2020-01-31 10:13:28 +0100

    add heroku config parts

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const IS_LOCAL = !process.env.PORT;
 const PORT = process.env.PORT || 5000;
 const ADDRESS = IS_LOCAL ? '127.0.0.1' : '0.0.0.0';
 
+const NS_ADDRESS = process.env.NS_ADDRESS || 'none';
+
 const fastify = require('fastify')({
   logger: IS_LOCAL
 });
 
 // Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.get('/config', function (request, reply) {
+  reply.send({
+    'NS_ADDRESS': NS_ADDRESS
+  })
 });
 
 fastify.listen(PORT, ADDRESS, function (err, address) {
