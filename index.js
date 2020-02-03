a0e84d0b42b06274563026b15b47d4a6b6877ffd
Reimar Metzen <reimar.metzen@iti-software.de>
2020-02-03 11:53:50 +0100

    test

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ const fastify = require('fastify')({
 });
 
 fastify.register(require('fastify-formbody'));
+fastify.addContentTypeParser('*', function (req, done) { 
+  var data = '';
+  req.on('data', chunk => { data += chunk });
+  req.on('end', () => { 
+    done(null, data) 
+  });
+});
 
 const getInput = (req, key) => {
   if (req.query && req.query[key] != null) {
